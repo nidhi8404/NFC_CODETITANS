40ebe33525b9012e97d79bfbb58c6242458d7dcb
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-09-16 01:01:09 +0530

    main dentist page

---
+++ Dentist.js
@@ -0,0 +1,20 @@
+export const FakeDoctors = [
+    {
+        "id": "001",
+        "name": "Prakash Joshi",
+        "status": "Ayurvedic Practitioner",
+        "img": "https://i.ibb.co/pz92gsK/d1.png"
+    },
+    {
+        "id": "002",
+        "name": "Darshan Desai",
+        "status": "Ayurvedic Practitioner",
+        "img": "https://i.ibb.co/yRcJ55b/d2.png"
+    },
+    {
+        "id": "003",
+        "name": "Neelam Chaturvedi",
+        "status": "Ayurvedic Practitioner",
+        "img": "https://i.ibb.co/zJXZcqW/d3.png"
+    }
+]
