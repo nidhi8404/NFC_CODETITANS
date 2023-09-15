a104d42a668a8d21d2aa378df0d94d60eb0b340a
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-09-16 00:01:50 +0530

    cross ref 2

---
+++ Treatment.js
@@ -0,0 +1,39 @@
+//cross reference page 2
+//treatments 
+export const FakeService = [
+    {
+        "id": "001",
+        "title": <span className="black-text"> Common Cold and Cough </span>,
+        "description":<span className="black-text"> Ayurveda recommends herbal remedies like Tulsi (Holy Basil), Ginger, and Honey for relieving symptoms. Warm herbal teas and steam inhalation with eucalyptus oil can provide much relief from nasal congestion.</span>,
+        "img": ""
+    },
+    {
+        "id": "002",
+        "title": <span className="black-text"> Arthritis </span>,
+        "description": <span className="black-text"> Ayurveda recommends herbal remedies like Boswellia, Turmeric, and Guggul for reducing inflammation and relieving joint pain. Panchakarma therapies such as Abhyanga and Janu Basti are also effective.</span>,
+        "img": "" },
+    {
+        "id": "003",
+        "title": <span className="black-text"> Digestive Disorders </span>,
+        "description": <span className="black-text"> Herbs like Ginger, Triphala, and Aloe Vera are used to improve digestion. Ayurvedic treatments include Panchakarma therapies like Virechana (therapeutic purgation) to detoxify the digestive system. </span>,
+        "img": ""
+    },
+    {
+        "id": "004",
+        "title": <span className="black-text"> Stress and Anxiety </span>,
+        "description": <span className="black-text"> Ayurveda suggests lifestyle modifications, meditation, and the use of herbs like Ashwagandha and Brahmi to reduce stress.  </span>,
+        "img": ""
+    },
+    {
+        "id": "005",
+        "title": <span className="black-text"> Diabetes </span>,
+        "description": <span className="black-text"> Ayurvedic herbs such as Bitter Melon, Gurmar, and Neem are used to control blood sugar levels. Dietary changes and regular exercise are also emphasized. </span>, 
+        "img": ""
+    },
+    {
+        "id": "006",
+        "title": <span className="black-text"> Skin Disorders </span>,
+        "description": <span className="black-text"> Ayurvedic remedies for skin conditions include Neem, Aloe Vera, and Manjistha. Ubtan and Ayurvedic oils are used to improve skin health. </span>,
+        "img": ""
+    }
+]
